d0b9aa6a3264c113ad65e8e5964ff209670de0c8
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2022-12-25 12:12:01 +0530

    debugging about me done

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -16,7 +16,7 @@ const About = () => {
     }, []) 
     return(
         <>
-        <div className="contianer about-page">
+        <div className="container about-page">
             <div className="text-zone">
                 <h1>
                     <AnimatedLetters
